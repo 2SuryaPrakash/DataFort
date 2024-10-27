374b2717d83f3b163cbfa4867269aa6a0558c907
viswa-dom <cs23bt076@iitdh.ac.in>
2024-10-27 19:14:20 +0530

    Minor update

--- index.js
+++ index.js
@@ -248,17 +248,17 @@ import crypto from "crypto";
 const app = express();
 const port = 3000;
 let code = 1000;
-let user_email="";
-let pubkey_arr=[];
-let prikey_arr=[];
-let use_name="";
-let user_pass="";
-let cids=[[]];
-let fileLinks=[];
-let walletId=0;
+let user_email = "";
+let pubkey_arr = [];
+let prikey_arr = [];
+let use_name = "";
+let user_pass = "";
+let cids = [[]];
+let fileLinks = [];
+let walletId = 0;
 // Get current directory name
 const __dirname = dirname(fileURLToPath(import.meta.url));
-const web3=new Web3();
+const web3 = new Web3();
 // Set the view engine to EJS
 app.set("view engine", "ejs");
 
@@ -280,8 +280,8 @@ const userSchema = new mongoose.Schema({
   password: { type: String, required: true },
   email: { type: String, unique: true, required: true },
   privatekey: { type: [String], unique: true, required: true },
-  publickey: {type: [String], unique: true, required: true },
-  cids: {type: [[String]], unique: true}
+  publickey: { type: [String], unique: true, required: true },
+  cids: { type: [[String]], unique: true }
 });
 
 const User = mongoose.model("User", userSchema);
@@ -294,9 +294,9 @@ const User = mongoose.model("User", userSchema);
 async function ipfsClient() {
   const ipfs = await create(
     {
-      host:"http://10.240.255.119",
-      port:5001,
-      protocol:"https"
+      host: "http://10.240.255.119",
+      port: 5001,
+      protocol: "https"
     }
   );
   return ipfs;
@@ -310,8 +310,8 @@ async function uploadToIpfs() {
 async function getFile(cid) {
   let ipfs = ipfsClient();
   let asyncitr = ipfs.cat(cid);
-  for await (const itr of asyncitr){
-    let data=Buffer.from(itr).toString();
+  for await (const itr of asyncitr) {
+    let data = Buffer.from(itr).toString();
     console.log(data);
   }
 }
@@ -335,9 +335,9 @@ app.get("/forgot_password", (req, res) => {
 });
 
 app.get("/upload_file", (req, res) => {
-  walletId=req.query.walletId;
-  if(isNaN(walletId)){
-    walletId=0;
+  walletId = req.query.walletId;
+  if (isNaN(walletId)) {
+    walletId = 0;
   }
   console.log(parseInt(walletId));
   res.render("upload", { name: use_name, cids: cids, walletno: walletId });
@@ -355,7 +355,7 @@ const handleLogin = (user, password, res, isAdmin) => {
     }
 
     if (result) {
-      user_pass=password;
+      user_pass = password;
       res.redirect(`${page}`);
     } else {
       res.render("login", { response: "Invalid Credentials. Try Again." });
@@ -363,8 +363,8 @@ const handleLogin = (user, password, res, isAdmin) => {
   });
 };
 
-function createNewKey(){
-  const keys=web3.eth.accounts.create();
+function createNewKey() {
+  const keys = web3.eth.accounts.create();
   return keys;
 }
 
@@ -372,7 +372,7 @@ function createNewKey(){
 app.post("/login", async (req, res) => {
   const { Username, Password } = req.body;
   const isAdmin = Username === "Admin@iitdh.ac.in";
-  use_name=Username;
+  use_name = Username;
   try {
     // Fetch user from the database
     const user = await User.findOne({ username: Username });
@@ -389,57 +389,57 @@ app.post("/login", async (req, res) => {
   }
 });
 
-app.post("/show_link", async (req,res)=>{
-  try{
-    fileLinks=[];
-    const self_user=await User.findOne({username: use_name});
-    const cid_arr=self_user.cids[walletId];
-    for(const cid of cid_arr){
-      const response=await fetch(`http://10.200.241.97:4000/api/file/${cid}`);
-      if(!response.ok){
+app.post("/show_link", async (req, res) => {
+  try {
+    fileLinks = [];
+    const self_user = await User.findOne({ username: use_name });
+    const cid_arr = self_user.cids[walletId];
+    for (const cid of cid_arr) {
+      const response = await fetch(`http://10.200.241.97:4000/api/file/${cid}`);
+      if (!response.ok) {
         throw new Error(`Failed to fetch link for ${cid}: ${response.statusText}`);
       }
       const contentType = response.headers.get('Content-Type');
       let fileExtension;
       switch (contentType) {
         case 'application/pdf':
-            fileExtension = 'pdf';
-            break;
+          fileExtension = 'pdf';
+          break;
         case 'image/jpeg':
-            fileExtension = 'jpg';
-            break;
+          fileExtension = 'jpg';
+          break;
         case 'image/png':
-            fileExtension = 'png';
-            break;
+          fileExtension = 'png';
+          break;
         case 'text/plain':
-            fileExtension = 'txt';
-            break;
+          fileExtension = 'txt';
+          break;
         case 'application/octet-stream':
-            fileExtension = 'bin'; // Generic binary file
-            break;
+          fileExtension = 'bin'; // Generic binary file
+          break;
         // Add more content types as needed
         default:
-            fileExtension = 'file'; // Fallback
+          fileExtension = 'file'; // Fallback
       }
       const fileUrl = `http://10.200.241.97:4000/api/file/${cid}`;
       fileLinks.push({ cid, filePath: fileUrl });
     }
-    res.render("upload",{name: use_name,cids: cids, walletno: walletId, fileLinks: fileLinks, response: ""});
+    res.render("upload", { name: use_name, cids: cids, walletno: walletId, fileLinks: fileLinks, response: "" });
   }
-  catch(err){
+  catch (err) {
     console.error('Error fetching file links:', err);
-    res.render("upload",{name: use_name,cids: cids, walletno: walletId, fileLinks: fileLinks, response: "No files have been uploaded"});
+    res.render("upload", { name: use_name, cids: cids, walletno: walletId, fileLinks: fileLinks, response: "No files have been uploaded" });
   }
 });
 
 // Signup Route
 app.post("/signup", async (req, res) => {
-  const username=req.body.name;
-  const email=req.body.mail;
-  const password=req.body.pswd;
-  const keys=createNewKey();
-  const prikey=keys.privateKey;
-  const pubkey=keys.address;
+  const username = req.body.name;
+  const email = req.body.mail;
+  const password = req.body.pswd;
+  const keys = createNewKey();
+  const prikey = keys.privateKey;
+  const pubkey = keys.address;
   // Log the incoming request data
   console.log("Signup request body:", req.body);
 
@@ -457,10 +457,10 @@ app.post("/signup", async (req, res) => {
 
     // Hash the password before storing
     const hashedPassword = await bcrypt.hash(password, 10);
-    prikey_arr=[prikey];
-    pubkey_arr=[pubkey];
+    prikey_arr = [prikey];
+    pubkey_arr = [pubkey];
     // Create and save a new user
-    const newUser = new User({ username: username, password: hashedPassword, email: email, privatekey: prikey_arr, publickey: pubkey_arr, cids: cids});
+    const newUser = new User({ username: username, password: hashedPassword, email: email, privatekey: prikey_arr, publickey: pubkey_arr, cids: cids });
     await newUser.save();
     res.redirect("/login"); // Redirect to login after successful signup
   } catch (err) {
@@ -469,25 +469,25 @@ app.post("/signup", async (req, res) => {
   }
 });
 
-async function encryptCID(cid){
+async function encryptCID(cid) {
   const iv = crypto.randomBytes(16);
-  const sel_user=await User.findOne({username: use_name});
-  const private_Key=sel_user.privatekey[walletId];
-  const cipher = crypto.createCipheriv('aes-256-cbc',private_Key,iv);
-  let encrypted = cipher.update(cid,'utf8','hex');
-  encrypted+=cipher.final('hex');
-  return{
+  const sel_user = await User.findOne({ username: use_name });
+  const private_Key = sel_user.privatekey[walletId];
+  const cipher = crypto.createCipheriv('aes-256-cbc', private_Key, iv);
+  let encrypted = cipher.update(cid, 'utf8', 'hex');
+  encrypted += cipher.final('hex');
+  return {
     encryptedCID: encrypted,
     iv: iv.toString('hex')
   };
 }
 
-async function decryptCID(encryptCID,ivHex) {
-  const ivBuffer = Buffer.from(ivHex,'hex');
-  const sel_user=await User.findOne({username: use_name});
-  const private_Key=sel_user.privatekey[walletId];
-  const decipher = crypto.createDecipheriv('aes-256-cbc',private_Key,ivBuffer);
-  let decrypted=decipher.update(encryptCID,'hex','utf8');
+async function decryptCID(encryptCID, ivHex) {
+  const ivBuffer = Buffer.from(ivHex, 'hex');
+  const sel_user = await User.findOne({ username: use_name });
+  const private_Key = sel_user.privatekey[walletId];
+  const decipher = crypto.createDecipheriv('aes-256-cbc', private_Key, ivBuffer);
+  let decrypted = decipher.update(encryptCID, 'hex', 'utf8');
   return decrypted;
 }
 app.post("/api/upload-cid", async (req, res) => {
@@ -495,7 +495,7 @@ app.post("/api/upload-cid", async (req, res) => {
   console.log("CID received:", cid);
 
   if (!cid) {
-      return res.status(400).send('CID is required');
+    return res.status(400).send('CID is required');
   }
 
   try {
@@ -508,18 +508,18 @@ app.post("/api/upload-cid", async (req, res) => {
     const sel_user = await User.findOne({ username: use_name });
 
     if (!sel_user) {
-        console.error(`User with username ${use_name} not found.`);
-        return res.status(404).send('User not found');
+      console.error(`User with username ${use_name} not found.`);
+      return res.status(404).send('User not found');
     }
 
     // Ensure `cids` is initialized as a 2D array
     if (!Array.isArray(sel_user.cids)) {
-        sel_user.cids = [];
+      sel_user.cids = [];
     }
 
     // Initialize array at `walletIndex` if it doesn’t exist
     if (!Array.isArray(sel_user.cids[walletIndex])) {
-        sel_user.cids[walletIndex] = [];
+      sel_user.cids[walletIndex] = [];
     }
 
     // Log cids structure before modification
@@ -535,37 +535,37 @@ app.post("/api/upload-cid", async (req, res) => {
     const savedUser = await sel_user.save();
 
     if (savedUser) {
-        console.log(`CID ${cid} added successfully to user ${use_name} under walletId ${walletIndex}`);
-        res.status(200).send('CID uploaded and saved successfully');
+      console.log(`CID ${cid} added successfully to user ${use_name} under walletId ${walletIndex}`);
+      res.status(200).send('CID uploaded and saved successfully');
     } else {
-        console.error("Failed to save user after CID update.");
-        res.status(500).send('Failed to save CID to database');
+      console.error("Failed to save user after CID update.");
+      res.status(500).send('Failed to save CID to database');
     }
   } catch (error) {
-      console.error('Error saving CID to database:', error);
-      res.status(500).send('Internal server error');
+    console.error('Error saving CID to database:', error);
+    res.status(500).send('Internal server error');
   }
 });
 
 
-app.post("/reset_password",async (req,res)=>{
+app.post("/reset_password", async (req, res) => {
   const r_email = req.body.email;
-  try{
-        const resobj = await User.findOne({email: r_email});
-        const result=resobj.email;
-        user_email=result;
-    if(result.length > 0){
-    code = Math.floor(Math.random()*10000);
-    let transporter = nodemailer.createTransport({
+  try {
+    const resobj = await User.findOne({ email: r_email });
+    const result = resobj.email;
+    user_email = result;
+    if (result.length > 0) {
+      code = Math.floor(Math.random() * 10000);
+      let transporter = nodemailer.createTransport({
         host: 'smtp.gmail.com',
         port: 465,
         secure: true,
         auth: {
-            user: 'thedominators4444@gmail.com',
-            pass: 'mlja cufb bgtt zkiq'
+          user: 'thedominators4444@gmail.com',
+          pass: 'mlja cufb bgtt zkiq'
         }
-    });
-    let mailOptions = {
+      });
+      let mailOptions = {
         from: '"The DOMinators" <thedominators4444@gmail.com>',
         to: r_email,
         subject: "Reset Password",
@@ -587,93 +587,94 @@ app.post("/reset_password",async (req,res)=>{
             </div>
         </body>
         </html>`,
-    };
-    transporter.sendMail(mailOptions,(error, info)=>{
-        if(error){
-            return console.log(error);
+      };
+      transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+          return console.log(error);
         }
-        res.render(__dirname+"/views/code.ejs");
-    });}
-    else{
-        res.render(__dirname+"/views/forgot_password.ejs",{
-            response: "User does not exist.",
-        });
-    }
-  }
-  catch(err){
-    console.error("Error finding user:", err.message);
-    res.render(__dirname+"/views/forgot_password.ejs",{
-      response: "User does not exist.",
-  });
-  }
-});
-
-app.post("/code",(req,res)=>{
-  const in_code=req.body.code;
-  if(in_code==code && in_code>=0){
-      res.sendFile(__dirname+"/views/reset.html");
-  }
-  else{
-      res.render(__dirname+"/views/code.ejs",{
-          response: "Incorrect Code",
+        res.render(__dirname + "/views/code.ejs");
       });
+    }
+    else {
+      res.render(__dirname + "/views/forgot_password.ejs", {
+        response: "User does not exist.",
+      });
+    }
+  }
+  catch (err) {
+    console.error("Error finding user:", err.message);
+    res.render(__dirname + "/views/forgot_password.ejs", {
+      response: "User does not exist.",
+    });
   }
 });
 
-app.post("/reset",async (req,res)=>{
-    const new_pass = req.body.Password;
-    try{
-      const hash = await bcrypt.hash(new_pass, 10);
-
-          // Update the password field
-          const result1 = await User.updateOne(
-            { email: user_email }, // Filter document by email
-            { $set: { password: hash } } // Update operation
-          );
-          if (result1.modifiedCount > 0) {
-            res.redirect("/");
-          } else {
-            res.status(400).send("Error updating password or user not found.");
-          }
-
-    }
-    catch(err){
-      console.error("Error: ", err);
-      res.status(500).send("Server Error");
-    }
+app.post("/code", (req, res) => {
+  const in_code = req.body.code;
+  if (in_code == code && in_code >= 0) {
+    res.sendFile(__dirname + "/views/reset.html");
+  }
+  else {
+    res.render(__dirname + "/views/code.ejs", {
+      response: "Incorrect Code",
+    });
+  }
 });
 
-app.post("/add",async (req,res)=>{
-  const keys=createNewKey();
-  const prikey=keys.privateKey;
-  const pubkey=keys.address;
-  const user=await User.findOne({username: use_name});
+app.post("/reset", async (req, res) => {
+  const new_pass = req.body.Password;
+  try {
+    const hash = await bcrypt.hash(new_pass, 10);
+
+    // Update the password field
+    const result1 = await User.updateOne(
+      { email: user_email }, // Filter document by email
+      { $set: { password: hash } } // Update operation
+    );
+    if (result1.modifiedCount > 0) {
+      res.redirect("/");
+    } else {
+      res.status(400).send("Error updating password or user not found.");
+    }
+
+  }
+  catch (err) {
+    console.error("Error: ", err);
+    res.status(500).send("Server Error");
+  }
+});
+
+app.post("/add", async (req, res) => {
+  const keys = createNewKey();
+  const prikey = keys.privateKey;
+  const pubkey = keys.address;
+  const user = await User.findOne({ username: use_name });
   user.privatekey.push(prikey);
   user.publickey.push(pubkey);
   await user.save();
   res.redirect("/profile");
 });
 
-app.get("/wallet",async (req,res)=>{
-  try{
-      const wallet = await User.findOne({username: use_name});
-      const no_of_keys=wallet.privatekey.length;
-      if(no_of_keys>1){
-        res.render("wallet",{wallets: no_of_keys});
-      }
-      else{
-        res.redirect('/upload_file');
-      }
+app.get("/wallet", async (req, res) => {
+  try {
+    const wallet = await User.findOne({ username: use_name });
+    const no_of_keys = wallet.privatekey.length;
+    if (no_of_keys > 1) {
+      res.render("wallet", { wallets: no_of_keys });
+    }
+    else {
+      res.redirect('/upload_file');
+    }
   }
-  catch(err){
+  catch (err) {
     res.redirect("/");
   }
 });
 
-app.get("/profile",async (req,res)=>{
-  const wallets1=await User.findOne({username: use_name}); 
-  user_email=wallets1.email;
-  res.render("profile",{
+app.get("/profile", async (req, res) => {
+  const wallets1 = await User.findOne({ username: use_name });
+  user_email = wallets1.email;
+  res.render("profile", {
     name: use_name,
     email: user_email,
     wallet: wallets1.publickey.length,
@@ -694,15 +695,15 @@ app.get('/get-private-key/:walletId', async (req, res) => {
   }
 });
 
-app.get("/logout",async (req,res)=>{
-  use_name="";
-  user_email="";
-  user_pass="";
-  pubkey_arr=[];
-  prikey_arr=[];
-  cids=[];
-  walletId=0;
-  fileLinks=[];
+app.get("/logout", async (req, res) => {
+  use_name = "";
+  user_email = "";
+  user_pass = "";
+  pubkey_arr = [];
+  prikey_arr = [];
+  cids = [];
+  walletId = 0;
+  fileLinks = [];
   res.redirect("/");
 });
 
